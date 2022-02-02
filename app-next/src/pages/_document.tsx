a9249aa8998f5eeae37de6577fb5d8251bd9e122
Colin Gray <colin@cgray.dev>
2022-02-02 12:48:03 -0800

    WIP: messing around with some layouts

--- app-next/src/pages/_document.tsx
+++ app-next/src/pages/_document.tsx
@@ -1,19 +1,25 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
+    const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps
+    return initialProps;
   }
 
   render() {
     return (
       <Html>
         <Head>
-        <link rel="shortcut icon" href="/favicon.ico"/>
+          <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
-        <body>
+        <body className="h-screen overflow-hidden flex items-center justify-center">
           <Main />
           <NextScript />
         </body>
