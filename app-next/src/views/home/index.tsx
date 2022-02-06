04f0eb06c4a5c875186014d4009bd4a8abef6288
Colin Gray <colin@cgray.dev>
2022-02-05 23:23:35 -0800

    Add image to landing page

--- app-next/src/views/home/index.tsx
+++ app-next/src/views/home/index.tsx
@@ -5,6 +5,7 @@ export const HomeView: FC = ({}) => {
     <div className="hero mx-auto p-4 min-h-16 py-4">
       <div className="hero-content flex flex-col max-w-lg">
         Welcome to the world of tomorrow!
+        <img src="/images/farm.jpg" className="h-40" />
       </div>
     </div>
   );
