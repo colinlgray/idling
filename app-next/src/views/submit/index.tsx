968da1b4ff3d3a1e08c592ac4499939483cd2f53
Colin Gray <colin@cgray.dev>
2022-02-10 16:55:21 -0800

    Add modal on submit page

--- app-next/src/views/submit/index.tsx
+++ app-next/src/views/submit/index.tsx
@@ -3,18 +3,42 @@ import { FC } from "react";
 
 export const SubmitView: FC = ({}) => {
   return (
-    <div className="hero mx-auto min-h-16">
-      <div className="hero-content flex flex-col max-w-lg justify-center">
-        <h2 className="text-xl">Show me what you got</h2>
-        <div className="container mx-auto ">
-          <div className="flex justify-center">
-            <img src="/images/show_me.png" className="h-40" />
+    <>
+      <div className="hero mx-auto min-h-16">
+        <div className="hero-content flex flex-col max-w-lg justify-center">
+          <h2 className="text-xl">Show me what you got</h2>
+          <div className="container mx-auto ">
+            <div className="flex justify-center py-2">
+              <img src="/images/show_me.png" className="h-40" />
+            </div>
+            <div className="flex justify-center">
+              <div>
+                <label
+                  htmlFor="my-modal-2"
+                  className="btn btn-primary modal-button"
+                >
+                  Submit for judging
+                </label>
+              </div>
+            </div>
           </div>
-          <button className="btn px-8 m-2 bg-blue-600 hover:bg-blue-800">
-            Submit tokens for judging
-          </button>
         </div>
       </div>
-    </div>
+
+      <input type="checkbox" id="my-modal-2" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <p>Judging period has not started yet</p>
+          <div className="modal-action">
+            <label htmlFor="my-modal-2" className="btn btn-primary">
+              Accept
+            </label>
+            <label htmlFor="my-modal-2" className="btn">
+              Close
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
